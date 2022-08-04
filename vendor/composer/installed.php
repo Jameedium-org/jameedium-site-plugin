fc069a99f661432370e994ad3c1cdb3b6e6819b9
amro-k-saleh <amrosaleh63@gmail.com>
2022-08-04 13:42:35 +0300

    composer autoload files auto generated

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'jameedium/jameedium-site-plugin',
+  ),
+  'versions' => 
+  array (
+    'jameedium/jameedium-site-plugin' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
